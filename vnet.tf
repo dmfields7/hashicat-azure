14caf7b5855ffaa721332578e40ab69220cf73ac
David Fields <dmfields7@yahoo.com>
2020-04-01 20:00:21 +0000

    added azure netywork module

--- vnet.tf
+++ vnet.tf
@@ -1,5 +1,5 @@
 module "network" {
-  source  = "app.terraform.io/dmfields-training/network/azurerm"
+  source  = "https://app.terraform.io/app/dmfields-training/modules/view/network/azurerm/3.0.1"
   version = "3.0.1"
   resource_group_name = "${var.prefix}-workshop"
 }
